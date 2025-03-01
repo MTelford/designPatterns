ec47019aa363d3c569a45f2513980ebdd7048799
MTelford <operator_141@outlook.com>
2025-03-01 15:39:20 +0000

    functional/streams practice

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,50 @@
-import StrategyPattern.BitcoinPaymentProcessor;
-import StrategyPattern.CreditCardPaymentProcessor;
-import StrategyPattern.ShoppingCart;
-import StrategyPatternFunctionalInterface.ShoppingCartFunctional;
-import StrategyPatternFunctionalInterface.StrategyFunctional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
-        BitcoinPaymentProcessor bitcoinPaymentProcessor = new BitcoinPaymentProcessor();
-        ShoppingCart shoppingCart = new ShoppingCart(bitcoinPaymentProcessor);
-        shoppingCart.makePayment(50);
+//        BitcoinPaymentProcessor bitcoinPaymentProcessor = new BitcoinPaymentProcessor();
+//        ShoppingCart shoppingCart = new ShoppingCart(bitcoinPaymentProcessor);
+//        shoppingCart.makePayment(50);
+//
+//        CreditCardPaymentProcessor creditCardPaymentProcessor = new CreditCardPaymentProcessor();
+//        shoppingCart.setPaymentStrategy(creditCardPaymentProcessor);
+//        shoppingCart.makePayment(75);
+//
+//        StrategyFunctional bitcoinPaymentProcessorFunc = (amount) -> System.out.println("Processed Bitcoin Payment $" + amount);
+//        StrategyFunctional creditCardPaymentProcessorFunc = (amount) -> System.out.println("Processed Credit Card Payment $" + amount);
+//
+//        ShoppingCartFunctional.processPayment((amount) -> System.out.println("Processed Bitcoin Payment $" + amount), 500);
+//        ShoppingCartFunctional.processPayment(creditCardPaymentProcessorFunc, 300);
 
-        CreditCardPaymentProcessor creditCardPaymentProcessor = new CreditCardPaymentProcessor();
-        shoppingCart.setPaymentStrategy(creditCardPaymentProcessor);
-        shoppingCart.makePayment(75);
 
-        StrategyFunctional bitcoinPaymentProcessorFunc = (amount) -> System.out.println("Processed Bitcoin Payment $" + amount);
-        StrategyFunctional creditCardPaymentProcessorFunc = (amount) -> System.out.println("Processed Credit Card Payment $" + amount);
+        Consumer<String> printer = System.out::println;
+//        printer.accept("Eureka!");
 
-        ShoppingCartFunctional.processPayment(bitcoinPaymentProcessorFunc, 500);
-        ShoppingCartFunctional.processPayment(creditCardPaymentProcessorFunc, 300);
-        }
+        Supplier<Double> supplier = Math::random;
+        supplier.get();
+        printer.accept(supplier.get().toString());
+
+        Function<Integer, Integer> function = (amount) -> (amount + 1);
+        Integer result = function.apply(50);
+        printer.accept(result.toString());
+
+
+        List<String> myListOfStrings = new ArrayList<>();
+        myListOfStrings.add("Alice");
+        myListOfStrings.add("Bob");
+        myListOfStrings.add("Janice");
+
+        Set<String> res = myListOfStrings.stream()
+                .filter((name) -> name.endsWith("ce"))
+                .collect(Collectors.toSet());
+
+        System.out.println(res);
+    }
     }
